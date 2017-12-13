167d0b001bbe73c447b58a6a8a2f7142bed9dbe1
Milan_K <allgemein@milan-kostic.de>
2017-12-13 13:00:54 +0100

    Initial Commit

---
+++ Aufgabe 4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Aufgabe 4
+//
+//  Created by Milan Kostic on 13.12.17.
+//  Copyright © 2017 Milan Kostic. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
